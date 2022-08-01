f9221c7479b41734f2dc7d495657d958e1d8c4d7
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-01 08:52:32 +0530

    ADDED: check the Employee is Present or Not

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,7 @@
 console.log("Welcome to Employee Wage");
+let checkEmp = Math.floor(Math.random()*2);
+if(checkEmp == 1){
+    console.log("Employee is present");
+}else{
+    console.log("Employee is Not Present");
+}
