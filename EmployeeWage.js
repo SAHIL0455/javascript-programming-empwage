606a201eae38942f5f0b9cd0f16a7d44767d5acb
SAHIL0455 <sahilpawar1004@gmail.com>
2022-08-01 09:24:14 +0530

    ADDED: Calculate the Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,7 +1,20 @@
 console.log("Welcome to Employee Wage");
-let checkEmp = Math.floor(Math.random()*2);
-if(checkEmp == 1){
-    console.log("Employee is present");
-}else{
-    console.log("Employee is Not Present");
+const Is_Part_Time = 1;
+const Is_Full_Time = 2;
+const Full_Time_Hr = 8;
+const Half_Time_Hr = 4;
+const Employee_Per_Hr_Rate = 20;
+let EmployeeWorkHr;
+let checkEmp = Math.floor(Math.random()*3);
+switch (checkEmp) {
+    case Is_Part_Time:
+        EmployeeWorkHr = Half_Time_Hr;
+        break;
+    case Is_Full_Time:
+        EmployeeWorkHr = Full_Time_Hr;
+        break;
+    default:
+        EmployeeWorkHr = 0;
 }
+let EmployeeWage = Employee_Per_Hr_Rate*EmployeeWorkHr;
+console.log("Employye Wage is "+EmployeeWage);
